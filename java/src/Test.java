6378fc5e8180523c5c7d52e13cc475121b2764b2
zhenxianyimeng <1920405993@qq.com>
2021-05-20 21:15:29 +0800

    init java proj

---
+++ java/src/Test.java
@@ -0,0 +1,9 @@
+/**
+ * @author zhenxianyimeng <1920405993@qq.com>
+ * Created on 2021-05-20
+ */
+public class Test {
+    public static void main(String[] args) {
+        System.out.println("--");
+    }
+}
